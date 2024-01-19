bccf58557ea22915d3519204001b4d0ebbbf5539
anordmann <anordmann@nomotech.com>
2024-01-19 09:07:22 +0100

    [FIX] No abstract class as an end class

--- slim-test/src/Application/Actions/Login/LoginAction.php
+++ slim-test/src/Application/Actions/Login/LoginAction.php
@@ -5,11 +5,10 @@ declare(strict_types=1);
 namespace App\Application\Actions\Login;
 
 use App\Application\Actions\Action;
-use App\Domain\User\UserRepository;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Log\LoggerInterface;
 
-abstract class LoginAction extends Action
+class LoginAction extends Action
 {
 	public function __construct(LoggerInterface $logger)
 	{
